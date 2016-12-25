ae5a0a41c10f42ce2dcbdb418d8b5d4cf3e46110
Juan Lopez <juan.lopez.metalero@gmail.com>
2016-12-25 19:13:09 +0100

    Upd

--- books.sh
+++ books.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -xv
 
+source ./settings.sh
+cd $base
+
 #[ -e /home/pi/bin/media-center-config ] && . /home/pi/bin/media-center-config
 
-base_books="/media/MULTIMEDIA/TORRENT/BOOKS"
 IFS=$'\n'
 date_str="$(date +%Y%m%d-%H_%M_%S)"
 #for j in $(find $base_books -type f -mmin +10 -size -20M -name "*epub") ; do
  #xvfb-run ebook-convert "$j" "$base_books/$(basename "$j" .epub).mobi"
 #done
 for j in $(find $base_books -type f -mmin +10 -size -20M -name "*mobi") ; do
- mpack -s "$(basename $j)" "$j" abadie25_51@kindle.com > /home/pi/bin/log/mail_books_${date_str}.txt 2>&1
- echo "BOOK $(basename $j) added" | mail -s "BOOK $(basename $j)" casape.tv1@gmail.com
+ mpack -s "$(basename $j)" "$j" $kindle_mail > /home/pi/bin/log/mail_books_${date_str}.txt 2>&1
+ echo "BOOK $(basename $j) added" | mail -s "BOOK $(basename $j)" $notify_mail
  mv "$j" "$base_books/../BOOKS_PROCESSED/$(basename $j)"
 done
 for j in $(find $base_books -type f -mmin +10 -size -20M -name "*epub") ; do
@@ -18,6 +20,6 @@ for j in $(find $base_books -type f -mmin +10 -size -20M -name "*epub") ; do
 done
 unset IFS
 
-#mpack -s "Hombres Buenos - Arturo Perez-Reverte" "/media/MULTIMEDIA/TORRENT/BOOKS/Hombres Buenos - Arturo Perez-Reverte.mobi" abadie25_51@kindle.com
+#mpack -s "Hombres Buenos - Arturo Perez-Reverte" "/media/MULTIMEDIA/TORRENT/BOOKS/Hombres Buenos - Arturo Perez-Reverte.mobi" $kindle_mail
 #ebook-convert Mi\ karma\ y\ yo\ -\ Marian\ Keyes.epub Mi\ karma\ y\ yo\ -\ Marian\ Keyes.mobi
 

028cd74183c82c906d74e77b80d239b9ec667aef
Juan Lopez <juan.lopez.metalero@gmail.com>
2016-12-26 08:31:03 +0100

    Upd

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,11 @@
 # Before executing this script please, install the latest full raspbian image from https://www.raspberrypi.org/downloads/raspbian/
 #[ -e $base_etc/media-center-config ] && source ./settings.sh ; sudo mkdir $base ; sudo chown pi:pi $base ; echo "source $base_etc/media-center-config" >> ~/.bashrc
 #[ ! -e ./settings.sh  ] && sudo mkdir /opt/rmc ; sudo chown pi:pi /opt/rmc ; cd /opt ; git clone https://github.com/keooek/rmc ; cd /opt/rmc
+
+mkdir -p $base_hd_input
+cd $base_hd_input
+mkdir ALL  AMULE  AMULE_TMP  BOOKS  BOOKS_PROCESSED  MOVIES-EN  MOVIES-SP  MP3  OTHERS  PS3  SHARE  SKIPPED  TORRENT_INCOMING  TORRENT_TMP  TVSHOWS-EN  TVSHOWS-SP
+
 apt-get update
 apt-get upgrade
 apt-get dist-upgrade

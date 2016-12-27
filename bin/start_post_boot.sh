e8cac0d719462a1b1de08f53f7f2efe550832abe
Juan Lopez <juan.lopez.metalero@gmail.com>
2016-12-28 00:30:23 +0100

    Upd

--- bin/start_post_boot.sh
+++ bin/start_post_boot.sh
@@ -1,8 +1,4 @@
 
-cd /home/pi/jenkins/
-#nohup ./maintenance_jenkins.sh &
-#nohup ./start_jenkins.sh &
-
 cd /home/pi/.aMule/
 killall amuled
 killall amuleweb
@@ -10,14 +6,10 @@ rm /home/pi/.aMule/{muleLock,statistics.dat}
 nohup /usr/bin/amuled &
 
 killall transmission-daemon
-/usr/bin/transmission-daemon -g /etc/transmission-daemon --log-debug -e $logs/transmission.log
+/usr/bin/transmission-daemon -g $base_sw/transmission --log-debug -e $logs/transmission.log
 
 sleep 30
 #$base_bin/forever_qbittorrent.sh
 nohup $base_bin/forever_amule.sh &
 nohup $base_bin/forever_transmission.sh &
-#bash -ic "pok"
 
-#/etc/init.d/transmission-daemon start
-#/etc/init.d/amule-daemon start
-#/etc/init.d/rtorrent-init start

4fae9c267170eee5ae343b6b618972ff1449bffd
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-05 12:46:01 +0100

    Upd

--- bin/media_automation.sh
+++ bin/media_automation.sh
@@ -24,7 +24,7 @@ for z in $(cd $rmc_base_hd_input/ALL/ ; ls -1t *.rar 2> /dev/null|grep -v ":") ;
  mv $rmc_base_hd_input/ALL/$z $rmc_base_hd_input/AUDIO
  if [ "$(unrar lt "$rmc_base_hd_input/AUDIO/$z" | egrep -i '(.mp3|.MP3)'|wc -m)" -gt 0 ] ; then
   mkdir -p "$rmc_base_hd_audio/$(basename $z .rar)"
-  unrar x "$rmc_base_hd_input/AUDIO/$z" "$rmc_base_hd_audio/$(basename $z .rar)"
+  unrar x -y "$rmc_base_hd_input/AUDIO/$z" "$rmc_base_hd_audio/$(basename $z .rar)"
   # If directory created has only one subdirectory and no other content, move the content one level before
   if [ "$(find "$rmc_base_hd_audio/$(basename $z .rar)" -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_audio/$(basename $z .rar)" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
    mv "$rmc_base_hd_audio/$(basename $z .rar)/*" $rmc_base_hd_audio
@@ -36,7 +36,7 @@ for y in $(cd $rmc_base_hd_input/ALL/ ; ls -1t *.zip 2> /dev/null|grep -v ":") ;
  mv $rmc_base_hd_input/ALL/$z $rmc_base_hd_input/AUDIO
  if [ "$(unzip -l "$rmc_base_hd_input/AUDIO/$y" | egrep -i '(.mp3|.MP3)' )" -gt 0 ] ; then
   mkdir -p "$rmc_base_hd_audio/$(basename $yi .zip)"
-  unzip "$rmc_base_hd_input/AUDIO/$y" -d "$rmc_base_hd_audio/$(basename $y .zip)"
+  unzip -o "$rmc_base_hd_input/AUDIO/$y" -d "$rmc_base_hd_audio/$(basename $y .zip)"
   # If directory created has only one subdirectory and no other content, move the content one level before
   if [ "$(find "$rmc_base_hd_audio/$(basename $y .zip)" -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_audio/$(basename $y .zip)" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
    mv "$rmc_base_hd_audio/$(basename $y .zip)/*" $rmc_base_hd_audio

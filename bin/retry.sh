2809af89a6d2f784aceeb64f820aa0b643d6f1ac
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-06 16:48:16 +0100

    Upd

--- bin/retry.sh
+++ bin/retry.sh
@@ -1,20 +1,8 @@
 #!/bin/bash -xv
-#Reorg downloaded mixed torrents in ALL y AMULE folder
-#MP3
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*\(MP3\|mp3\).*' -exec mv {} $rmc_base_hd_input/MP3 \;
-#PS3
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*\(PS3\|ps3\).*' -exec mv {} $rmc_base_hd_input/PS3 \;
-#TVShows EN
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*S[0-3][0-9]E[0-3][0-9].*' -exec mv {} $rmc_base_hd_input/TVSHOWS-EN \;
-#TVShows SP
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*[0-2]?[0-9]x[0-2][0-9].*' -exec mv {} $rmc_base_hd_input/TVSHOWS-SP \;
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*[^0-9][0-9]x?[0-2][0-9][^0-9].*' -exec mv {} $rmc_base_hd_input/TVSHOWS-SP \;
-#Movies ES
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*\(panish\|astellano\|spa.ol\|PANISH\|ASTELLANO\|SPA.OL\).*' -exec mv {} $rmc_base_hd_input/MOVIES-SP \;
-#Movies EN
-find $rmc_base_hd_input/SKIPPED -maxdepth 1 -regex '.*\(1080p\|720p\).*' -exec mv {} $rmc_base_hd_input/MOVIES-EN \;
+#Retry again stuff in SKIPPED
+mv -vf $rmc_base_hd_input/SKIPPED $rmc_base_hd_input/ALL
 
-#Purgar el lock por si se queda pillado
-rm -f $rmc_base/sw/flexget/.config-lock
+#Purge lock flexget
+rm -f $rmc_base/sw/flexget/.config*lock
 
 exit 0

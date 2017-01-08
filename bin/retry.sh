fac9d4dc86d0c5633f87c1078e72337846dd5aed
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-08 07:31:06 +0100

    Upd

--- bin/retry.sh
+++ bin/retry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 #Retry again stuff in SKIPPED
-mv -vf $rmc_base_hd_input/SKIPPED $rmc_base_hd_input/ALL
+mv -vf $rmc_base_hd_input/SKIPPED/* $rmc_base_hd_input/ALL
 
 #Purge lock flexget
 rm -f $rmc_base/sw/flexget/.config*lock

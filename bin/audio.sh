e3af8fb64cbb9df0efccebdfb9e1b41464d5082d
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-15 11:14:29 +0100

    Upd

--- bin/audio.sh
+++ bin/audio.sh
@@ -7,6 +7,13 @@ date_str="$(date +%Y%m%d-%H_%M_%S)"
 for d in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type d ! -name tmp ) ; do 
  rm -rf $rmc_base_hd_input/AUDIO/tmp/*
  filebot.sh --log all --log-file $rmc_logs/audio_dir_rename_${date_str}.txt --action $action --output "$rmc_base_hd_input/AUDIO/tmp" -script fn:amc $d --conflict override -non-strict --def music=y "musicFormat={artist}/{album}/{artist}-{album}-{media.TrackPosition.pad(2)}-{t}"
+ if [[ "$(grep -i "Failed to identify or process any files" $rmc_logs/audio_dir_rename_${date_str}.txt)" != "" ]] ; then
+  filebot.sh --log all --log-file $rmc_logs/audio_acoustid_${date_str}.txt -rename --output "$rmc_base_hd_audio/UNCATALOGED/" --db AcoustID "musicFormat={artist}/{album}/{artist}-{album}-{media.TrackPosition.pad(2)}-{t}"
+  rm -rf $rmc_base_hd_input/AUDIO_PROCESSED/$(basename $d)*
+  [ ! -f "$d.*" ] && mv -v ${d} $rmc_base_hd_input/AUDIO_PROCESSED
+  rm -rf ${d} ; mv -v ${d}.* $rmc_base_hd_input/AUDIO_PROCESSED
+  exit 0
+ fi
  # If directory created has only one subdirectory and no other content, then it's a one artist album, if not it's a compilation
  if [ "$(find "$rmc_base_hd_input/AUDIO/tmp/" -mindepth 1 -maxdepth 2 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
   cp -vrf $rmc_base_hd_input/AUDIO/tmp/* "$rmc_base_hd_audio/UNCATALOGED/"

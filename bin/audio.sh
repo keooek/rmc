c500e4b4dc25d329a658fe3fba31037394125126
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-15 08:40:45 +0100

    Upd

--- bin/audio.sh
+++ bin/audio.sh
@@ -8,7 +8,7 @@ for d in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type d ! -nam
  rm -rf $rmc_base_hd_input/AUDIO/tmp/*
  filebot.sh --log all --log-file $rmc_logs/audio_dir_rename_${date_str}.txt --action $action --output "$rmc_base_hd_input/AUDIO/tmp" -script fn:amc $d --conflict override -non-strict --def music=y "musicFormat={artist}/{album}/{artist}-{album}-{media.TrackPosition.pad(2)}-{t}"
  # If directory created has only one subdirectory and no other content, then it's a one artist album, if not it's a compilation
- if [ "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
+ if [ "$(find "$rmc_base_hd_input/AUDIO/tmp/" -mindepth 1 -maxdepth 2 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
   cp -vrf $rmc_base_hd_input/AUDIO/tmp/* "$rmc_base_hd_audio/UNCATALOGED/"
   rm -rf $rmc_base_hd_input/AUDIO/tmp/*
  else

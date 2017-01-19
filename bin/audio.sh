2c8e9df0b0177ff1de368fb4d6f0417909417932
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-01-19 16:14:10 +0100

    Upd

--- bin/audio.sh
+++ bin/audio.sh
@@ -8,7 +8,7 @@ for d in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type d ! -nam
  rm -rf $rmc_base_hd_input/AUDIO/tmp/*
  filebot.sh --log all --log-file $rmc_logs/audio_dir_rename_${date_str}.txt --action $action --output "$rmc_base_hd_input/AUDIO/tmp" -script fn:amc $d --conflict override -non-strict --def music=y "musicFormat={artist}/{album}/{artist}-{album}-{media.TrackPosition.pad(2)}-{t}"
  if [[ "$(grep -i "Failed to identify or process any files" $rmc_logs/audio_dir_rename_${date_str}.txt)" != "" ]] ; then
-  ls -1R $d | mail -s "Added $(basename $d)" $rmc_notify_mail 
+  ls -1R $d | mail -s "Added $(ls -1 $rmc_base_hd_input/AUDIO/tmp/*)" $rmc_notify_mail 
   mv $d $rmc_base_hd_audio/UNCATALOGED/
   #filebot.sh --log all --log-file $rmc_logs/audio_acoustid_${date_str}.txt -rename --output "$rmc_base_hd_audio/UNCATALOGED/" --db AcoustID "musicFormat={artist}/{artist}-{media.TrackPosition.pad(2)}-{t}" 
   #Arreglar bug filebot para albumArtist https://raw.githubusercontent.com/cowmix88/Filebot-Scripts/master/lib/acoustid.groovy
@@ -20,7 +20,7 @@ for d in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type d ! -nam
  fi
  # If directory created has only one subdirectory and no other content, then it's a one artist album, if not it's a compilation
  if [ "$(find "$rmc_base_hd_input/AUDIO/tmp/" -mindepth 1 -maxdepth 2 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
-  ls -1R $rmc_base_hd_input/AUDIO/tmp | mail -s "Added $(basename $rmc_base_hd_input/AUDIO/tmp/* )" $rmc_notify_maim
+  ls -1R $rmc_base_hd_input/AUDIO/tmp | mail -s "Added $(ls -1 mc_base_hd_input/AUDIO/tmp/*)" $rmc_notify_maim
   cp -vrf $rmc_base_hd_input/AUDIO/tmp/* "$rmc_base_hd_audio/UNCATALOGED/"
   rm -rf $rmc_base_hd_input/AUDIO/tmp/*
  else
@@ -30,7 +30,7 @@ for d in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type d ! -nam
   filebot.sh --log all --log-file $rmc_logs/audio_dir_va_rename_${date_str}.txt --action $action --output "$rmc_base_hd_input/AUDIO/tmp/" -script fn:amc $d --conflict override -non-strict --def music=y "musicFormat={album}/{media.TrackPosition.pad(2)}-{artist}-{t}"
   #Si aun asi no se organiza se para de intentar ya que no parece un album corriente o un recopilatorio reconocido
   if [ "$(find "$rmc_base_hd_input/AUDIO/tmp/" -mindepth 1 -maxdepth 2 -type d -printf 1 | wc -m)" -eq 2 -a "$(find "$rmc_base_hd_input/AUDIO/tmp/" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
-   ls -1R $rmc_base_hd_input/AUDIO/tmp | mail -s "Added $(basename $rmc_base_hd_input/AUDIO/tmp/* )" $rmc_notify_maim
+   ls -1R $rmc_base_hd_input/AUDIO/tmp | mail -s "Added $(ls -1 $rmc_base_hd_input/AUDIO/tmp/* )" $rmc_notify_maim
    cp -vrf $rmc_base_hd_input/AUDIO/tmp/* "$rmc_base_hd_audio/UNCATALOGED/"
    rm -rf $rmc_base_hd_input/AUDIO/tmp/*
   else
@@ -48,6 +48,6 @@ done
 #For regular files
 for f in $(find $rmc_base_hd_input/AUDIO/ -mindepth 1 -maxdepth 1 -type f -name "*.mp3") ; do
  filebot.sh --log all --log-file $rmc_logs/audio_file_rename_${date_str}.txt --action $action --output "$rmc_base_hd_audio/UNCATALOGED/" -script fn:amc $f --conflict override -non-strict --def music=y "musicFormat={artist}-{album}-{media.TrackPosition.pad(2)}-{t}"
- grep mp3 $rmc_logs/audio_file_rename_${date_str}.txt | mail -s "Added mp3 file" $rmc_notify_mail
+ grep mp3 $rmc_logs/audio_file_rename_${date_str}.txt | mail -se "Added mp3 file" $rmc_notify_mail
  [ -f "$d" ] && mv $f $rmc_base_hd_input/AUDIO_PROCESSED
 done

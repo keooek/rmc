add93cc81e5aab43e7d47e31026cfbfa13e84222
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-05-18 16:13:21 +0200

    kodi17 and exit menu ssh

--- bin/delete_tvshows_watched_lang.sh
+++ bin/delete_tvshows_watched_lang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 
-sqlite3 ~/.kodi/userdata/Database/MyVideos93.db "select episode.c18 from episode, files where episode.idFile = files.idFile and files.playcount not null order by episode.c18;" > $rmc_base/tmp/tvshows_delete.tmp
+sqlite3 ~/.kodi/userdata/Database/MyVideos107.db "select episode.c18 from episode, files where episode.idFile = files.idFile and files.playcount not null order by episode.c18;" > $rmc_base/tmp/tvshows_delete.tmp
 grep "TVSHOWS-SP" $rmc_base/tmp/tvshows_delete.tmp | grep -i -v -f $rmc_base/etc/tvshows_sp_not_delete.txt > $rmc_base/tmp/tvshows_delete.final
 grep "TVSHOWS-EN" $rmc_base/tmp/tvshows_delete.tmp | grep -i -v -f $rmc_base/etc/tvshows_en_not_delete.txt >> $rmc_base/tmp/tvshows_delete.final
 while read line ; do
--- bin/manage_system_menu.sh
+++ bin/manage_system_menu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 while true; do
- echo -e "Select: \n 1)Amule_Restart \n 2)Transmission_Restart \n 3)Kodi_Restart \n 4)Kodi_Stop \n 5)Kodi_Start \n 6)Init_0 \n 7)Init_6 \n 8)List automated tvshows spanish \n 9)List automated tvshows english \n a)Manage_tvshows.sh \n b)Manage_tvshows_not_delete.sh \n m)Local Menu \n q)quit \n x)Logout"
+ echo -e "Select: \n 1)Amule_Restart \n 2)Transmission_Restart \n 3)Kodi_Restart \n 4)Kodi_Stop \n 5)Kodi_Start \n 6)Init_0 \n 7)Init_6 \n 8)List automated tvshows spanish \n 9)List automated tvshows english \n a)Manage_tvshows.sh \n b)Manage_tvshows_not_delete.sh \n m)Local Menu \n q)quit \n x)Logout "
  read n
  case $n in
     1) cd /home/pi/.aMule/;killall amuled;killall amuleweb;rm /home/pi/.aMule/{muleLock,statistics.dat};nohup /usr/bin/amuled & ;;
     2) killall transmission-daemon; /usr/bin/transmission-daemon -g $rmc_base/sw/transmission --log-debug -e $rmc_logs/transmission.log ;;
-    3) killall kodi kodi.bin ; sudo /etc/init.d/lightdm stop ; sudo /etc/init.d/lightdm start ;;
-    4) killall kodi kodi.bin ; mv /home/pi/.config/autostart/kodi.desktop /home/pi/.config/ ;sudo /etc/init.d/lightdm stop ; sudo /etc/init.d/lightdm start ;;
+    3) killall kodi kodi_v7.bin ; sudo /etc/init.d/lightdm stop ; sudo /etc/init.d/lightdm start ;;
+    4) killall kodi kodi_v7.bin ; mv /home/pi/.config/autostart/kodi.desktop /home/pi/.config/ ;sudo /etc/init.d/lightdm stop ; sudo /etc/init.d/lightdm start ;;
     5) mv /home/pi/.config/kodi.desktop /home/pi/.config/autostart/kodi.desktop ; sudo /etc/init.d/lightdm stop ; sudo /etc/init.d/lightdm start ;;
     6) sudo init 0;;
     7) sudo init 6;;

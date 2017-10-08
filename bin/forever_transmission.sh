e585acce80f8ffd1c0f0b2aaa374c8d323bfa663
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-10-08 14:23:12 +0200

    forever_trans config dir fix

--- bin/forever_transmission.sh
+++ bin/forever_transmission.sh
@@ -6,7 +6,7 @@ while true; do
  pids_old=`ps ax |grep "/usr/bin/transmission-daemon"|grep -v grep | awk '{print $1}'`
  if [ -z "$pids_old" ]; then
   echo "$(date): Reinicio transmission-daemon" > $rmc_logs/forever_transmission_$(date +%Y%m%d-%H_%M_%S).txt 2>&1
-  /usr/bin/transmission-daemon -g /etc/transmission-daemon --log-debug -e $rmc_logs/transmission.log
+  /usr/bin/transmission-daemon -g $rmc_base/sw/transmission --log-debug -e $rmc_logs/transmission.log
  fi
  sleep 60
 done

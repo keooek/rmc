6993a4a24416d787942b03bbb8cef6562cf38809
Juan Lopez <juan.lopez.metalero@gmail.com>
2017-12-28 23:58:16 +0100

    fix forever amuleweb

--- bin/forever_amule.sh
+++ bin/forever_amule.sh
@@ -9,7 +9,7 @@ while true; do
 
  if [ $? -ne 0 ] # if not running
  then
-  echo "$(date): Reinicio amule" > $rmc_logs/forever_amule_$(date +%Y%m%d-%H_%M_%S).txt 2>&1
+  echo "$(date): Reinicio amule" > $rmc_logs/forever_amuled_$(date +%Y%m%d-%H_%M_%S).txt 2>&1
   cd /home/pi/.aMule/
   killall amuled
   killall amuleweb
@@ -18,4 +18,18 @@ while true; do
  fi
 
  sleep 60
+
+ # find pid
+ var=$(/usr/bin/pgrep -c amuleweb)
+
+ if [ $? -ne 0 ] # if not running
+ then
+  echo "$(date): Reinicio amule" > $rmc_logs/forever_amuleweb_$(date +%Y%m%d-%H_%M_%S).txt 2>&1
+  cd /home/pi/.aMule/
+  #killall amuled
+  killall amuleweb
+  #rm /home/pi/.aMule/{muleLock,statistics.dat}
+  nohup amuleweb --amule-config-file=/home/pi/.aMule/amule.conf &
+ fi
+
 done
